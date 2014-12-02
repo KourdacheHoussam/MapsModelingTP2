83e857b9afd7f1fa8548dbf980ef86d672889857
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-02 21:06:01 +0100

    Add of Test.java and map.xmi(instance of ecore model)

---
+++ MapsModelingTP2/src/tp/houssam/test/Test.java
@@ -0,0 +1,54 @@
+package tp.houssam.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import maps.MapsPackage;
+import maps.map;
+
+import org.eclipse.emf.common.util.*;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLMapImpl;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//Je charge l'instance map.xmi du méta-modèle maps.ecore
+		Resource resource = chargerModele("model/map.xmi", MapsPackage.eINSTANCE);
+		if (resource == null) System.err.println(" Erreur de chargement du modèle");
+		
+		
+		//Instruction récupérant le modèle sous forme d'arbre a  partir de la classe racine "map"
+		map maMap = (map) resource.getContents().get(0);
+						
+		System.out.println(maMap.getName());
+				
+		
+	}
+	
+	
+	public static Resource chargerModele(String uri, EPackage pack) {
+		   Resource resource = null;
+		   try {
+		      URI uriUri = URI.createURI(uri);
+		      Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		      resource = (new ResourceSetImpl()).createResource(uriUri);
+		      XMLResource.XMLMap xmlMap = new XMLMapImpl();
+		      xmlMap.setNoNamespacePackage(pack);
+		      Map options = new HashMap();
+		      options.put(XMLResource.OPTION_XML_MAP, xmlMap);
+		      resource.load(options);
+		   }
+		   catch(Exception e) {
+		      System.err.println("ERREUR chargement du modÃ¨le : "+e);
+		      e.printStackTrace();
+		   }
+		   return resource;
+		}
+
+}
