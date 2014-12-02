473e078c53e744bc52b5d3c37557aeda4cbe031d
HoussamKourdache <kourdache88@yahoo.fr>
2014-12-02 20:43:05 +0100

    Génération code source depuis un model ecore chargé

---
+++ MapsModelingTP2/src/maps/impl/SquareImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package maps.impl;
+
+import maps.MapsPackage;
+import maps.Square;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Square</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class SquareImpl extends PublicSpaceImpl implements Square {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected SquareImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MapsPackage.Literals.SQUARE;
+	}
+
+} //SquareImpl
